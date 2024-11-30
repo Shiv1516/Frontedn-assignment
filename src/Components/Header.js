28bf1aa932701c77a68ca920c7669180e7cdcb59
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-11-30 14:14:09 +0530

    all changes done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,21 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { IoMdCall } from "react-icons/io";
 import { MdMail } from "react-icons/md";
+import React, { useState } from "react";
 
-function Header() {
+const Header = () => {
+  const [dropdownOpen, setDropdownOpen] = useState({
+    locations: false,
+    experiences: false,
+    offers: false,
+  });
+
+  const toggleDropdown = (menu) => {
+    setDropdownOpen((prev) => ({
+      ...prev,
+      [menu]: !prev[menu],
+    }));
+  };
   return (
     <>
       <div className="header-section bg3">
@@ -19,14 +31,78 @@ function Header() {
         </div>
       </div>
       <div className="">
-        <div className="navbar df fww jcsb wrapper ptb12">
+        <nav className="navbar df v-center jcsb ptb12 wrapper">
           <div className="logo">
             <img src="images/logo.png" alt="logo" className="logo" />
           </div>
-        </div>
+          <div className="df aic">
+            <ul className="nav-links v-center">
+              <li
+                className="nav-item cp ptb16 fc-h3 transit2 mlr12 plr16 bg-h1 br8"
+                onMouseEnter={() => toggleDropdown("locations")}
+                onMouseLeave={() => toggleDropdown("locations")}
+              >
+                Locations
+                {dropdownOpen.locations && (
+                  <ul className="dropdown bg1 pa br8 shadow-lg fc2 brd2 ofh">
+                    <li className="dropdown-item ptb16 plr24 cp wsnw fc-h2 bg-h2">
+                      New York
+                    </li>
+                    <li className="dropdown-item ptb16 plr24 cp wsnw fc-h2 bg-h2">
+                      Paris
+                    </li>
+                    <li className="dropdown-item ptb16 plr24 cp wsnw fc-h2 bg-h2">
+                      Tokyo
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li
+                className="nav-item cp ptb16 fc-h3 transit2 mlr12 plr16 bg-h1 br8"
+                onMouseEnter={() => toggleDropdown("experiences")}
+                onMouseLeave={() => toggleDropdown("experiences")}
+              >
+                Experiences
+                {dropdownOpen.experiences && (
+                  <ul className="dropdown bg1 pa br8 shadow-lg fc2 brd2 ofh">
+                    <li className="dropdown-item ptb16 plr24 cp wsnw fc-h2 bg-h2">
+                      Adventure
+                    </li>
+                    <li className="dropdown-item ptb16 plr24 cp wsnw fc-h2 bg-h2">
+                      Luxury
+                    </li>
+                    <li className="dropdown-item ptb16 plr24 cp wsnw fc-h2 bg-h2">
+                      Relaxation
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li
+                className="nav-item cp ptb16 fc-h3 transit2 mlr12 plr16 bg-h1 br8"
+                onMouseEnter={() => toggleDropdown("offers")}
+                onMouseLeave={() => toggleDropdown("offers")}
+              >
+                Offers
+                {dropdownOpen.offers && (
+                  <ul className="dropdown bg1 pa br8 shadow-lg fc2 brd2 ofh">
+                    <li className="dropdown-item ptb16 plr24 cp wsnw fc-h2 bg-h2">
+                      Seasonal Discounts
+                    </li>
+                    <li className="dropdown-item ptb16 plr24 cp wsnw fc-h2 bg-h2">
+                      Flash Sales
+                    </li>
+                  </ul>
+                )}
+              </li>
+            </ul>
+            <button className="cta-btn bg2 fc1 plr16 h48 cp br8 fw6">
+              Sign In
+            </button>
+          </div>
+        </nav>
       </div>
     </>
   );
-}
+};
 
 export default Header;
